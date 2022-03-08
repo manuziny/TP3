6b7fc63db00845f8351d3a55134b1221b71c7906
manuziny <geoojv@gmail.com>
2022-03-08 19:25:43 -0300

    estacao finalizada
    objeto finalizado

--- TP3Inicial/src/Estacao.java
+++ TP3Inicial/src/Estacao.java
@@ -8,10 +8,6 @@ public class Estacao {
     //Deixar um numero fixo de vagas e de bicicletas já existentes setados
     //o nome ja vai ser setado e o ID também.
     //acertar com o matix a qntd de bicicletas cadastradas depois.
-    Estacao e1 = new Estacao();
-    Estacao e2 = new Estacao();
-    Estacao e3 = new Estacao();
-    Estacao e4 = new Estacao();
 
     private String id;
     private String nomeEstacao;
@@ -34,48 +30,20 @@ public class Estacao {
         return this.qntdBicicletas;
     }
 
-    public void situacaoEstacoes(){
-        e1.id = "001";
-        e1.nomeEstacao = "Mane Garrincha";
-        e1.qntdBicicletas = 10;
-        e1.qntdVagas = 15;
-
-        e2.id = "235";
-        e2.nomeEstacao = "Arniqueiras";
-        e2.qntdBicicletas = 8;
-        e2.qntdVagas = 10;
-
-        e3.id = "897";
-        e3.nomeEstacao = "Furnas";
-        e3.qntdBicicletas = 12;
-        e3.qntdVagas = 3;
-
-        e4.id = "478";
-        e4.nomeEstacao = "Taguapark";
-        e4.qntdBicicletas = 20;
-        e4.qntdVagas = 8;
-    }
-
-    public void buscaEstacao(ArrayList<String> id, ArrayList<String> nomeEstacao, ArrayList<String> qntdBicicletas, ArrayList<String> qntdVagas){
+    public void buscaEstacao(ArrayList<String> id, ArrayList<String> nomeEstacao, int qntdBicicletas, int qntdVagas){
         String palavraBusca;
         Scanner ler = new Scanner(System.in);
 
-        System.out.println("Digite o ID ou nome da estacao que deseja procurar: ");
+        System.out.println("Digite o nome da estacao que deseja procurar: ");
         palavraBusca = ler.nextLine();
 
-        if(palavraBusca == e1.nomeEstacao || palavraBusca == e1.id){
-            System.out.println("\nID: " + e1.id + "\nEstacao: " + e1.nomeEstacao + "\nBicicletas disponiveis: " + e1.qntdBicicletas + "\nVagas disponiveis: " + e1.qntdVagas);
-
-        }else if(palavraBusca == e2.nomeEstacao || palavraBusca == e2.id){
-            System.out.println("\nID: " + e2.id + "\nEstacao: " + e2.nomeEstacao + "\nBicicletas disponiveis: " + e2.qntdBicicletas + "\nVagas disponiveis: " + e2.qntdVagas);
-
-        }else if(palavraBusca == e3.nomeEstacao || palavraBusca == e3.id){
-            System.out.println("\nID: " + e3.id + "\nEstacao: " + e3.nomeEstacao + "\nBicicletas disponiveis: " + e3.qntdBicicletas + "\nVagas disponiveis: " + e3.qntdVagas);
-
-        }else if(palavraBusca == e4.nomeEstacao || palavraBusca == e4.id){
-            System.out.println("\nID: " + e4.id + "\nEstacao: " + e4.nomeEstacao + "\nBicicletas disponiveis: " + e4.qntdBicicletas + "\nVagas disponiveis: " + e4.qntdVagas);
-
+        for(int i = 0; i < nomeEstacao.size(); i++){
+            if(palavraBusca.equals(nomeEstacao.get(i))){
+                System.out.println("\nID: " + id.get(i) + "\nEstacao: " + nomeEstacao.get(i) + "\nBicicletas disponiveis: " + qntdBicicletas + "\nVagas disponiveis: " + qntdVagas);
+                System.out.println("\n");
+            }
         }
+
     }
 
 }
